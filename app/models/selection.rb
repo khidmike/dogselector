6b5a72da59d3739e08aefd39e37c262228f3e73f
Michael Khidekel <michael.khidekel@gmail.com>
2015-07-23 17:01:29 -0400

    more styling

--- app/models/selection.rb
+++ app/models/selection.rb
@@ -10,7 +10,7 @@ class Selection < ActiveRecord::Base
   validates :need_for_attention, presence: true
 
   def choices
-    Breed.where("size = ? AND kid_friendly >= ? AND pet_friendly >= ? AND grooming <= ? AND trainability >= ? AND space <= ? AND activity_level = ? AND need_for_attention = ?", size, kid_friendly, pet_friendly, grooming, trainability, space, activity_level, need_for_attention)
+    Breed.where("size = ? AND kid_friendly >= ? AND pet_friendly >= ? AND grooming <= ? AND trainability >= ? AND space <= ? AND activity_level = ? AND need_for_attention = ?", size, kid_friendly, pet_friendly, grooming, trainability, space, activity_level, need_for_attention).limit(5)
   end
 
 end
